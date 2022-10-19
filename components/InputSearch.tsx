6bea14975834879e3f7e27d27fabb1b381e0f084
Fabrizio Onorio <onoriofabrizio@gmail.com>
2022-10-19 16:13:52 +0200

    changed input color

--- components/InputSearch.tsx
+++ components/InputSearch.tsx
@@ -18,7 +18,7 @@ const InputSearch = ({
 				type="text"
 				value={packageName}
 				onChange={(e) => setPackageName(e.target.value)}
-				className="border border-red-900 rounded-md p-2"
+				className="border border-black-900 rounded-md p-2"
 			/>
 		</form>
 	);
