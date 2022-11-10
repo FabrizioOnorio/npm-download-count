8ae6e7fb636e72a902cdb61c24c7e76cabced3d0
Fabrizio Onorio <onoriofabrizio@gmail.com>
2022-11-10 16:00:58 +0100

    finishe to fix routing handling in case of presence of user or not

--- pages/favourites.tsx
+++ pages/favourites.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from "next";
-import react from "react";
+import react, { useEffect } from "react";
 import FavouritePage from "../components/FavouritePage";
+import { useUser } from "@auth0/nextjs-auth0";
+import { useRouter } from "next/router";
 
 export interface IFavouriteObj {
 	downloadsData: object[];
@@ -9,15 +11,28 @@ export interface IFavouriteObj {
 	numberDownloadsMonthly: number;
 	numberDownloadsWeekly: number;
 	infos: { description: string; homepage: string };
-  id : string;
+	id: string;
 }
 
 interface IFavouritesProps {
 	favourites: IFavouriteObj[];
 }
 
-export const Favourites: NextPage<IFavouritesProps> = ({favourites}) => {
-	return <FavouritePage favourites={favourites} />;
+export const Favourites: NextPage<IFavouritesProps> = ({ favourites }) => {
+	 const { user, isLoading } = useUser();
+		const router = useRouter();
+
+		useEffect(() => {
+			if (!(user || isLoading)) {
+				router.push("/");
+			}
+		}, [user, isLoading, router]);
+
+	return (
+		<div style={{display: user ? 'block': 'none'}}>
+			<FavouritePage favourites={favourites} />
+		</div>
+	);
 };
 
 export default Favourites;
